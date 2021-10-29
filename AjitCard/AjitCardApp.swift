855c4ea7119a73aa069b3bd40e8b7e7275e05dd3
Ajit Kumar Behera <nitajitrkl@gmail.com>
2021-10-29 16:15:16 +0530

    Initial Commit

---
+++ AjitCard/AjitCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AjitCardApp.swift
+//  AjitCard
+//
+//  Created by ajit behera on 29/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AjitCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

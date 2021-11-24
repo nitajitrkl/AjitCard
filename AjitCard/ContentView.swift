5a5bd3f6602969e875d3ee9f7ba758e627765d76
Ajit Behera <41351154+nitajitrkl@users.noreply.github.com>
2021-11-24 15:51:21 +0530

    init

--- AjitCard/ContentView.swift
+++ AjitCard/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("ajit")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Ajit Behera")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                infoView(text: "+91 9178627122", imageName: "phone.fill")
+                infoView(text: "nitajitrkl@gmail.com", imageName: "envelope.fill")
+            }
+        }
     }
 }
 
@@ -19,3 +41,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
